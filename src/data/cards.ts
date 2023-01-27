5326677afba02a5cdb1d89ec46a17dde02994a18
chicocalves <fcoocf@hotmail.com>
2023-01-27 00:41:35 -0300

    initial

---
+++ src/data/cards.ts
@@ -0,0 +1,18 @@
+export const cards = [
+  {
+    id: '1',
+    back: 'Cachorro'
+  },
+  {
+    id: '2',
+    back: 'Ovelha'
+  },
+  {
+    id: '3',
+    back: 'Gato'
+  },
+  {
+    id: '4',
+    back: 'Cavalo'
+  },
+];
